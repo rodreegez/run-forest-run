1ca6c8e43a0df0f4df41d921a124677a9a3d5020
Adam <electronicbattleweapon7@gmail.com>
2013-12-07 17:18:50 +0000

    start and stop the sequencer

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,40 @@
-console.log('\'Allo \'Allo!');
+var context = new webkitAudioContext();
+
+// create a new step sequencer with parameters:
+// (audio context, step length, sequence of frequences)
+var sequencer = new StepSequencer(context, 1, [440, 660, 440, 400]);
+
+// method to inject other audio nodes at the end of the graph
+sequencer.setupAudio();
+
+var Player = function(sequencer) {
+  this.playing = false;
+  this.sequencer = sequencer;
+}
+
+Player.prototype.play = function() {
+  this.playing = true;
+  sequencer.play();
+}
+
+Player.prototype.stop = function() {
+  this.playing = false;
+  sequencer.stop();
+}
+
+p = new Player(sequencer)
+
+
+var $button = $('.control');
+
+$button.on('click', function(ev) {
+  ev.preventDefault();
+
+  if (p.playing) {
+    p.stop();
+    $button.text("Play!");
+  } else {
+    p.play()
+    $button.text("Stop!")
+  }
+})
